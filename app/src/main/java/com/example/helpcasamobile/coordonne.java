f0d0a103bd60e50026aef2d6d5976000f4285a2e
samali-rayen <115874034+samali-rayen@users.noreply.github.com>
2024-05-09 23:34:40 +0100

    third

---
+++ app/src/main/java/com/example/helpcasamobile/coordonne.java
@@ -0,0 +1,14 @@
+package com.example.helpcasamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coordonne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordonne);
+    }
+}
