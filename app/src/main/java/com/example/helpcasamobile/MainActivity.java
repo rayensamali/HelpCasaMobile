4db27335d262984db564649fe69bb4fccc4e2a69
samali-rayen <115874034+samali-rayen@users.noreply.github.com>
2024-05-09 23:40:09 +0100

    third1

--- app/src/main/java/com/example/helpcasamobile/MainActivity.java
+++ app/src/main/java/com/example/helpcasamobile/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.helpcasamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView identifier,creecompte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        creecompte = findViewById(R.id.createacc);
+        identifier = findViewById(R.id.identif);
+        identifier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,sidentifier.class));
+            }
+        });
+        creecompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,createacc.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/helpcasamobile/createacc.java
+++ app/src/main/java/com/example/helpcasamobile/createacc.java
@@ -2,13 +2,33 @@ package com.example.helpcasamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 public class createacc extends AppCompatActivity {
+    private RadioGroup rdg;
+    private TextView button;
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createacc);
+        rdg = findViewById(R.id.radg);
+        button = findViewById(R.id.suivant);
+        i = new Intent(createacc.this,);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if(rdg.getCheckedRadioButtonId()==R.id.agentimmobilier){
+
+               }
+            }
+        });
+
     }
 }
