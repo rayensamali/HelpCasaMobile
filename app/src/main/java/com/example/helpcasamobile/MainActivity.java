c4a78e2dc47058b85418e0f04bba504558aaa7bf
Mehdi <mehdisaadoun30@gmail.com>
2024-05-20 13:19:38 +0100

    rayen

--- app/src/main/java/com/example/helpcasamobile/MainActivity.java
+++ app/src/main/java/com/example/helpcasamobile/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.helpcasamobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private TextView identifier,creecompte;
+private TextView identifier,creecompte,apropos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,7 @@ private TextView identifier,creecompte;
 
         creecompte = findViewById(R.id.createacc);
         identifier = findViewById(R.id.identif);
+        apropos = findViewById(R.id.Aprop);
         identifier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +29,13 @@ private TextView identifier,creecompte;
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,createacc.class));
+            }
+        });
+        apropos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.seloger.com/"));
+                startActivity(intent);
             }
         });
     }
