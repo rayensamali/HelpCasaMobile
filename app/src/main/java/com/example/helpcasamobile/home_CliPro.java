49e7f12c2d74076d7df0efcad097c340f8468a54
rayen <rayen007samali@gmail.com>
2024-12-25 14:57:50 +0100

    second commint

--- app/src/main/java/com/example/helpcasamobile/home_CliPro.java
+++ app/src/main/java/com/example/helpcasamobile/home_CliPro.java
@@ -3,70 +3,51 @@ package com.example.helpcasamobile;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-public class home_CliPro extends AppCompatActivity {
+public class home_CliPro extends AppCompatActivity  implements View.OnClickListener {
 
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     private ImageButton param;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_cli_pro);
 
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        findViewById(R.id.poster).setOnClickListener(this);
+        findViewById(R.id.consulter).setOnClickListener(this);
+        findViewById(R.id.hist).setOnClickListener(this);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
 
-        param = findViewById(R.id.paramButton); // Replace with your ImageButton ID
-       // param.setOnClickListener(v -> startActivity(new Intent(home_CliPro.this, SettingsActivity.class))); // Adjust activity
+
+
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.poster){
+            startActivity(new Intent(home_CliPro.this, nouvelleAnnonce.class));
+        }
+        if(view.getId() == R.id.consulter){
+            startActivity(new Intent(home_CliPro.this, annonce_cli_prp.class));
+        }
+
+        if(view.getId() == R.id.hist){
+            startActivity(new Intent(home_CliPro.this, historique.class));
+        }
+    }
     @SuppressLint("MissingSuperCall")
     @Override
     public void onBackPressed() {
-        finishAffinity(); // Terminates all activities
+        // Fermer l'application lorsque le bouton "Back" est pressé
+        finishAffinity(); // Termine toutes les activités et quitte l'application
     }
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.postAnn) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.)
-            return true;
-        } else if (item.getItemId() == R.id.consulterAnn) {
-            startActivity(new Intent(this, annonce_cli_prp.class));
-            return true;
-        } else if (item.getItemId() == R.id.historique) {
-            startActivity(new Intent(this, historique.class));
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
 }
